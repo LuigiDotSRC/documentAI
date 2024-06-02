afcc95d55cead9cbd5325b2cc4529436d240cc2d
LuigiDotSRC <luigi.terashima@gmail.com>
2024-06-02 19:29:23 -0400

    Add finished home page

--- frontend/src/components/navbar.svelte
+++ frontend/src/components/navbar.svelte
@@ -2,11 +2,11 @@
 
 </script>
 
-<div class="flex bg-neutral-700 py-2.5 px-2.5 justify-between">
-    <a class="text-3xl font-light bg-gradient-to-r from-indigo-500 from-10% via-sky-500 via-30% to-emerald-500 to-90% text-white" href="/">DocumentAI</a>
+<div class="flex bg-neutral-800 py-2.5 px-2.5 justify-between">
+    <a class="text-3xl font-light bg-gradient-to-r from-indigo-500 from-10% via-sky-500 via-30% to-emerald-500 to-90% text-white ml-7" href="/">DocumentAI</a>
 
     <div class="flex items-center">
-        <a href="/" class="text-xl text-white mx-2.5">Vector Stores</a>
-        <a href="/" class="text-xl text-white mx-2.5">Conversations</a>
+        <a href="/" class="text-xl text-white mx-2.5 hover:text-blue-400 hover:underline">Vector Stores</a>
+        <a href="/" class="text-xl text-white mx-2.5 hover:text-blue-400 hover:underline">Conversations</a>
     </div>
 </div>
--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -2,6 +2,20 @@
     import Navbar from "../components/navbar.svelte";
 </script>
 
-<Navbar />
-<h1>Welcome to SvelteKit</h1>
-<p class="text-green-400">Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<main class="bg-neutral-700">
+    <Navbar />
+    <div class="ml-10 my-2">
+        <h3 class="text-xl text-neutral-400">Welcome To</h3>
+        <h1 class="text-4xl">DocumentAI</h1>
+    </div>
+
+    <div class="ml-10 my-10">
+        <p class="my-4">A webapp interface to chat with your documents. Powered by Flask, Svelte, TailwindCSS, and OpenAI's APIs.</p>
+        <p class="mt-8 mb-2">To get started:</p>
+        <ol>
+            <li class="ml-8"><span class="text-slate-400 mr-2">1.</span> Ensure that your OpenAI API Key is set in the .env file</li>
+            <li class="ml-8"><span class="text-slate-400 mr-2">2.</span> Upload your files to a <a href="/" class="text-blue-400 underline hover:text-blue-600">Vector Store</a></li>
+            <li class="ml-8"><span class="text-slate-400 mr-2">3.</span> Start a <a href="/" class="text-blue-400 underline hover:text-blue-600">Conversation</a> with your files</li>
+        </ol>
+    </div>
+</main>
