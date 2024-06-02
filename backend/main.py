d05f4a85178d5849abaed9c4e55800e418b00e35
LuigiDotSRC <luigi.terashima@gmail.com>
2024-06-02 11:03:52 -0400

    Add SvelteKit frontend framework

--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, flash, request, redirect, url_for
+from flask import Flask, render_template, flash, request, redirect, url_for, CORS
 from werkzeug.utils import secure_filename
 from openai import OpenAI
 from dotenv import load_dotenv
---
+++ frontend/src/components/navbar.svelte
@@ -0,0 +1,12 @@
+<script>
+
+</script>
+
+<div class="flex bg-neutral-700 py-2.5 px-2.5 justify-between">
+    <a class="text-3xl font-light bg-gradient-to-r from-indigo-500 from-10% via-sky-500 via-30% to-emerald-500 to-90% text-white" href="/">DocumentAI</a>
+
+    <div class="flex items-center">
+        <a href="/" class="text-xl text-white mx-2.5">Vector Stores</a>
+        <a href="/" class="text-xl text-white mx-2.5">Conversations</a>
+    </div>
+</div>
